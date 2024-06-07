5106d9f1702e837349e05ff02c7ddc612871524a
csehgabor451 <csehgabor451@gmail.com>
2024-06-07 20:15:51 +0000

    before the posts

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Gabriel de Czech';
+    : 'Gabriel Cseh';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Python Tutorial Point';
